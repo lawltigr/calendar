51f3190b960479cf21afc51c4f5514985ceb6dd6
lawltigr <vasilisatikhonova110@gmail.com>
2026-08-03 21:18:06 -0700

    Update forms.py

--- forms.py
+++ forms.py
@@ -20,3 +20,11 @@ class LoginForm(FlaskForm):
 class CourseForm(FlaskForm):
     title = StringField("Course title", validators=[DataRequired(), Length(max=150)])
     description=TextAreaField("Description", validators=[Length(max=2000)])
+class SessioForm(FlaskForm):
+    start_time = DateTimeLocalField("Beginning", format="Y-%m-%dT%H:$M", validators=[DataRequired()])
+    end_time = DateTimeLocalField("End", format="Y-%m-%dT%H:$M", validators=[DataRequired()])
+    location = StringField("Place of the event", validators=[Length(max=150)])
+    capacity = IntegerField("Capacity (spots)", validators=[DataRequired(), NumberRange(max=1000)])
+    def validate_end_time(self, field):
+        if self.start_time.data and field.data <= self.start_time.data:
+            raise ValidationError("The ending time must be later than the start time")
