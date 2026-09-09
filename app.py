e9c52632fb12925099bcfd3d51a65791b115f475
lawltigr <vasilisatikhonova110@gmail.com>
2026-09-08 20:18:40 -0700

    updatee

--- app.py
+++ app.py
@@ -28,4 +28,4 @@ def create_app(config_class=Config):
 
 app = create_app()
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    app.run(debug=True, host="0.0.0.0", port=5001)
